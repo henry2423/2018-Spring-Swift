be2ff08c8486b05ed3c05f381d3c783b7442f97d
Henry <henry2423@gmail.com>
2018-06-24 00:23:46 +0800

    update mini-project 17
    feature including Self-sizing Table View Cells, dynamic font type

---
+++ 18-Artistry-Starter/Artistry/ArtistTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ArtistTableViewCell.swift
+//  Artistry
+//
+//  Created by Henry on 2018/6/23.
+//  Copyright © 2018 RayWenderlich. All rights reserved.
+//
+
+import UIKit
+
+class ArtistTableViewCell: UITableViewCell {
+
+  @IBOutlet weak var bioLabel: UILabel!
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var artistImageView: UIImageView!
+  
+
+}
---
+++ 18-Artistry-Starter/Artistry/WorkTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  WorkTableViewCell.swift
+//  Artistry
+//
+//  Created by Henry on 2018/6/23.
+//  Copyright © 2018 RayWenderlich. All rights reserved.
+//
+
+import UIKit
+
+class WorkTableViewCell: UITableViewCell {
+
+  @IBOutlet weak var workImageView: UIImageView!
+  @IBOutlet weak var workTitleLabel: UILabel!
+  @IBOutlet weak var moreInfoTextView: UITextView!
+}
